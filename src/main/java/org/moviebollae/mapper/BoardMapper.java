e2641ab2e0a0a22c2e385d25746e2259980f9fca
EunJuOh33 <eunjuoh.dev@gmail.com>
2021-01-22 18:19:01 +0900

    feat/board-mapper : mapper 인터페이스에 게시글 조회 sql 작성, 출력 테스트 완료

---
+++ src/main/java/org/moviebollae/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package org.moviebollae.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.moviebollae.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+
+}
